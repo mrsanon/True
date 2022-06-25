4fec95c7931fddf21d5503848d4a2fcc7129c645
mrsanon <106508721+mrsanon@users.noreply.github.com>
2022-06-25 23:44:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Created By LoRD.
-   * Contact Me on wa.me/919778383987
-   * Follow https://github.com/Lord-official
+   * Created By mrsanon
+   * Contact Me on wa.me/94766816809
+   * Follow https://github.com/mrsanon
 **/
 
 const fs = require('fs')
@@ -18,27 +18,27 @@ global.APIKeys = {
 }
 //======================================//
 
-global.wm = '«𝒍𝒐𝒓𝒅.𝒐𝒇𝒄»🇮'
+global.wm = '🇱🇰𝚖𝚛.𝚜𝚊𝚗𝚘𝚗'
 global.ucpn = ''
 
 //================= Url =================//
-global.linkgc = 'https://chat.whatsapp.com/CUCsW6BWfmJLJwJgPQIaKM'
-global.linkig = 'https://www.instagram.com/reel/Ccx79E0A8Rt/?igshid=YmMyMTA2M2Y='
-global.linkyt = 'https://youtu.be/RnpyRe_7jZA'
-global.linkfb = 'https://fb.watch/aYv0jAffAO'
-global.web = 'https://raselcomel.github.io'
-global.web1 = 'https://anu.rasell.repl.co'
-global.linkgh = 'https://github.com/Lord-official'
+global.linkgc = 'https://chat.whatsapp.com/IfrEWJ30nUd3Cbi3uIvtIb'
+global.linkig = 'https://www.instagram.com/reel/Ccx79E0A8Rt/?igshid=YmMyMTA2M='
+global.linkyt = 'https://youtu.be/RnpyRe_7A'
+global.linkfb = 'https://fb.watch/aYv0jfAO'
+global.web = 'https://Thisan04.github.io'
+global.web1 = 'https://.repl.co'
+global.linkgh = 'https://github.com/mrsanon'
 
 global.session = process.env.SESSION_ID|| ''
 global.titlink = 'Folllow me on Instagram'
 global.bodlink = 'Lord-official'
 // Other
-global.watermark = '«𝒍𝒐𝒓𝒅.𝒐𝒇𝒄»🇮🇳'
-global.owner = ['919778383987']
-global.premium = ['919778383987']
-global.packname = '𝐋𝐨𝐑𝐃-𝐌𝐃'
-global.author = '𝐿𝑜𝑟𝑑-𝑜𝑓𝑓𝑖𝑐𝑖𝑎𝑙'
+global.watermark = '🇱🇰MR.SANON'
+global.owner = ['94766816809']
+global.premium = ['94766816809']
+global.packname = '𝘼𝙎𝙄𝘼𝙏𝘼 𝙈𝘿'
+global.author = '𝚃𝙷𝙸𝚂𝙰𝙽'
 global.sessionName = 'lord'
 global.prefa = ['','!','.','🐦',',','*']
 global.sp = '❄︎'
